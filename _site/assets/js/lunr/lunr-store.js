d2ac2475fbf6c9fd015fe68039b96abbe2d872d5
Alexander Ladd <xander@alexanders-mbp.lan>
2021-09-02 17:31:53 -0700

    update website by removing old blogs and adding description

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,16 +4,4 @@ var store = [{
         "tags": [],
         "url": "/jekyll/update/blogger/",
         "teaser": "/assets/images/trees.jpeg"
-      },{
-        "title": "What’s not on a Grad School App",
-        "excerpt":"As somone who has applied two consecutive years to graduate schools, I’ve put a lot of thought about what I can and can’t communicate on my personal statement. At first, two great pieces of advice as far as writing a personal statement appear to conflict with eachother – 1. tell...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/grad1/",
-        "teaser": "/assets/images/trees.jpeg"
-      },{
-        "title": "Am I a Blogger?",
-        "excerpt":"I am in the process of making my own personal website and as I am stapling posters to the walls and setting up furniture, I can’t help but think about how important the task at hand is. At the same time, I can’t help but think about equally unimportant it...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/Am-I-a-Blogger/",
-        "teaser": "/assets/images/trees.jpeg"
       }]

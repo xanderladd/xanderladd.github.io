28067c7e11b3f5cde43bcdc81c9917482edf1d6b
Alexander Ladd <xander@alexanders-mbp.lan>
2021-11-21 21:30:56 -0800

    added review #2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,8 +5,8 @@ var store = [{
         "url": "/reviews/review1/",
         "teaser": "/assets/images/trees.jpeg"
       },{
-        "title": "Review 1",
-        "excerpt":"Review 1 : In Silico by Dr. Fairhall Citation: Fairhall, Adrienne L. “In silico: where next?.” Eneuro 8.2 (2021). Intro This writing starts out by laying out the context of 2013, a very important year for funding large neuroscience research initiatives, the U.S. BRAIN projects and the European Human Brain...","categories": ["reviews"],
+        "title": "Review 2",
+        "excerpt":"Sparse balance: excitatory-inhibitory networks with small bias currents and broadly distributed synaptic weights by Ramin Khajeh, Francesco Fumarola, Larry F. Abbott Citation: Khajeh, Ramin, Francesco Fumarola, and L. F. Abbott. “Sparse balance: excitatory-inhibitory networks with small bias currents and broadly distributed synaptic weights.” bioRxiv (2021). DISCLAIMER I am very interested...","categories": ["reviews"],
         "tags": [],
         "url": "/reviews/review2/",
         "teaser": "/assets/images/trees.jpeg"

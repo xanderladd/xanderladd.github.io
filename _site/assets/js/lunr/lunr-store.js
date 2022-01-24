256c289915862848bdea59cb204a6bc9d4a70bfa
Alexander Ladd <xander@alexanders-mbp.lan>
2022-01-23 20:13:08 -0800

    added blog 11

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -70,4 +70,10 @@ var store = [{
         "tags": [],
         "url": "/reviews/review10/",
         "teaser": "/assets/images/trees.jpeg"
+      },{
+        "title": "Review 11: The Bandwagon",
+        "excerpt":"The Bandwagon by Claude E. Shannon Citation: Shannon, Claude E. “The bandwagon.” IRE transactions on Information Theory 2.1 (1956): 3. In the intersection of probability, statistics, and engineering, information theory has become applied ubiquitously throughout a wide range of research. In the 1940s, Claude Shannon conducted some of the most...","categories": ["reviews"],
+        "tags": [],
+        "url": "/reviews/review11/",
+        "teaser": "/assets/images/trees.jpeg"
       }]
